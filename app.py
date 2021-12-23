7875ca9ee37d62010feb97ab9600c8087c978d99
Luis Felipe Vidal Arellano <larellano@SF.PREFEITURA.SP.GOV.BR>
2021-12-23 16:06:36 -0300

    Load inicial

--- app.py
+++ app.py
@@ -1,13 +1,72 @@
-from flask import Flask, render_template
+##### Code inspired from video by Cairocoders https://www.youtube.com/watch?v=z3109C_xDP8
+##### If you're going to use this code for a user table, don't forget to encrypt the passwords!
+
+from flask import Flask, render_template, redirect, request, url_for, flash
 import myFuncs
 
 app = Flask(__name__)
 
+##Página de entrada para ver a tabela de usuarios.
 @app.route('/')
 def index():
     res = myFuncs.selecionaTodosUsuarios()
-    print(res)
     return render_template('index.html', lista_usuarios = res)
 
+##Método para adicionar usuário.
+@app.route('/addUser', methods=['POST'])
+def addUser():
+    if request.method == 'POST':
+        login = request.form['flogin']
+        email = request.form['femail']
+        senha = request.form['fpass']
+        res = myFuncs.addUsuario(login, email, senha)
+        print(res)
+        if res == True:
+            flash("Usuário adicionado com sucesso.")
+            return redirect(url_for('index'))
+        elif res == False:
+            flash("Login já em uso. Favor escolher outro.")
+            return redirect(url_for('index'))
+        else:
+            flash("Houve um erro na operação addUser.")
+            return redirect(url_for('index'))
+
+##Método para deletar usuário.
+@app.route('/delete/<id>', methods=['GET'])
+def deleteUsuario(id):
+    if request.method =='GET':
+        if myFuncs.deleteUsuario(id) == True:
+            flash("Usuário deletado com sucesso.")
+            return redirect(url_for('index'))
+        else:
+            flash("Houve um erro na operação deleteUser.")
+            return redirect(url_for('index'))
+
+##Método para exibir formulário para editar usuário.
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def editUsuario(id):
+    user = myFuncs.selectUsuario(id)
+    return render_template('edit.html', user = user)
+
+##Método para editar usuário.
+@app.route('/update/<id>', methods=['POST', 'GET'])
+def updateUsuario(id):
+     if request.method == 'POST':
+        login = request.form['flogin']
+        email = request.form['femail']
+        senha = request.form['fpass']
+        res = myFuncs.updateUsuario(id, login, email, senha)
+        if res == True:
+            flash("Usuário editado com sucesso.")
+            return redirect(url_for('index'))
+        elif res == False:
+            flash("Login já em uso. Favor escolher outro.")
+            return redirect(url_for('index'))
+        else:
+            flash("Houve um erro na operação UpdateUser.")
+            return redirect(url_for('index'))
+
+##Método main para rodar app.
 if __name__ == '__main__':
+    app.secret_key = 'secret'
     app.run(debug=True)
--- myFuncs.py
+++ myFuncs.py
@@ -1,4 +1,6 @@
+from logging import log
 import sqlite3
+import hashlib
 
 DB_NAME = 'dbTeste.sqlite'
 
@@ -11,9 +13,78 @@ def conectaDB():
         return "Houve um erro na conexão com o banco de dados."
 
 def selecionaTodosUsuarios():
-    conn = conectaDB()
-    cur = conn.cursor()
-    query = 'SELECT * from users'
-    cur.execute(query)
-    res = cur.fetchall()
-    return res
+    try:
+        conn = conectaDB()
+        cur = conn.cursor()
+        query = 'SELECT * from users'
+        cur.execute(query)
+        res = cur.fetchall()
+        conn.close()
+        return res
+    except:
+        return "Houve um erro ao consultar o banco de dados."
+
+def addUsuario(login, email, senha):
+    try:
+        conn = conectaDB()
+        cur = conn.cursor()
+        if checkLoginUnico(login) == True:
+            query = 'INSERT INTO users (login, email, senha) VALUES (?, ?, ?)'
+            cur.execute(query, [login, email, senha])
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            return False
+    except:
+        return "Houve um erro ao adicionar um usuário ao banco de dados."
+
+def deleteUsuario(id):
+    try:
+        conn = conectaDB()
+        cur = conn.cursor()
+        query = 'DELETE FROM users where id=?'
+        cur.execute(query,[id])
+        conn.commit()
+        conn.close()
+        return True
+    except:
+        return "Houve um erro ao deletar um usuário do banco de dados."
+
+def selectUsuario(id):
+    try:
+        conn = conectaDB()
+        cur = conn.cursor()
+        query = 'SELECT * from users where id=?'
+        cur.execute(query, [id])
+        res = cur.fetchone()
+        conn.close()
+        return res
+    except:
+        return "Houve um erro ao selecionar o usuario no banco de dados."
+    
+def updateUsuario(id, login, email, senha):
+    try:
+        conn = conectaDB()
+        cur = conn.cursor()
+        if checkLoginUnico(login) == True:
+            query = 'UPDATE users SET login=?, email=?, senha=? where id=?'
+            cur.execute(query, [login, email, senha, id])
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            return False
+    except:
+        return "Houve um erro ao editar o usuario no banco de dados."
+
+def checkLoginUnico(login):
+        conn = conectaDB()
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM users WHERE login='{0}'".format(login))
+        numIguais = cur.fetchone()[0]
+        if numIguais == 0:
+            return True
+        else:
+            return False
+    
